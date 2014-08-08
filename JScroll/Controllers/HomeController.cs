0ffaabff5b43887bad5e6e387815f188ceea1130
Madan Mohan Venkat Baggu <madanvenkat535@gmail.com>
2014-08-08 15:40:29 +0530

    intial commit
    intial commit

---
+++ JScroll/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JScroll.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Users(int pageID)
+        {
+           UserModel model = new UserModel();
+
+            //Get users from the database
+           List<Users> usersList = new List<Users>();
+           usersList.Add(new Users { UserName = pageID.ToString() });
+
+           model.Users = usersList;
+
+           //increment page count here
+           model.PageCount = pageID + 20;
+
+            //return model to partialView
+
+           return PartialView("_UsersPartial", model);
+        }
+    }
+
+
+    public class UserModel
+    {
+        public List<Users> Users { get; set; }
+        public int PageCount { get; set; }
+    }
+
+    public class Users
+    {
+        public string UserName { get; set; }
+    }
+}
---
+++ JScroll/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JScroll.Startup))]
+namespace JScroll
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
